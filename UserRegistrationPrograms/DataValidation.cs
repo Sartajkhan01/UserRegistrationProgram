d43e527db7b348fd9c9e59043c6dc1ad550c4ddd
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2022-12-14 22:35:24 +0530

    Validating first Name

---
+++ UserRegistrationPrograms/DataValidation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationPrograms
+{
+    public class DataValidation
+    {
+        public Regex First_Name_Regex = new Regex(@"^[A-Z]{1}[A-Za-z]{2,}?$");
+        public void ValidateFirstName()
+        {
+            Console.Write("Enter First Name : ");
+            string firstName = Console.ReadLine();
+
+            if (First_Name_Regex.IsMatch(firstName))
+            {
+                Console.WriteLine("The First Name Is Valid ");
+            }
+            else
+            {
+                Console.WriteLine("The First Name Is Not Valid");
+            }
+        }
+    }
+}
